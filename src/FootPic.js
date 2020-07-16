87c1043fe561bab9208dc750dc00be0898a0fd13
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2020-07-16 16:58:37 +0200

    Add link to buy buttons

--- src/FootPic.js
+++ src/FootPic.js
@@ -7,7 +7,11 @@ const FootPic = (props) => (
         <img alt={props.name} src={props.pic}/>
         <h4>{props.name}</h4>
         <div className={'price'}>
-        <strong>420.69 kr</strong> <button>Buy now</button>
+                <strong>420.69 kr</strong>
+                <a style={{display: 'flex'}} href={'https://www.systembolaget.se/dryck/aperitif-dessert/caprice-kir-296601'}>
+                        <button>Buy now</button>
+                </a>
+
         </div>
     </div>
 )
