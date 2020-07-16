7726860131957c55a1f8899cd15fa976607330d6
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2020-07-16 12:12:05 +0200

    Copy correct directory during deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ podman run -it --rm -v ./:/usr/src/app:Z -w /usr/src/app node npm install
 podman run -it --rm -v ./:/usr/src/app:Z -w /usr/src/app node npm run build
 
 rm -rf /var/www/smultronfeet.nu
-cp -a public /var/www/smultronfeet.nu
+cp -a build /var/www/smultronfeet.nu
