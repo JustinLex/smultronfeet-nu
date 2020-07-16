8af9b3f699fde9a802c9daef9ff1f887d9e25039
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2020-07-16 12:09:25 +0200

    Fix deploy script npm install

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-podman run -it --rm -v ./:/usr/src/app:Z -w /usr/src/app node 'npm install && npm run build'
+podman run -it --rm -v ./:/usr/src/app:Z -w /usr/src/app node npm install
+podman run -it --rm -v ./:/usr/src/app:Z -w /usr/src/app node npm run build
 
 rm -rf /var/www/smultronfeet.nu
 cp -a public /var/www/smultronfeet.nu
