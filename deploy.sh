ab09edeb39de71f719c63b7fafc01b816f5a6784
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2020-07-16 12:44:31 +0200

    dont copy selinux context

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ podman run -it --rm -v ./:/usr/src/app:Z -w /usr/src/app node npm install
 podman run -it --rm -v ./:/usr/src/app:Z -w /usr/src/app node npm run build
 
 rm -rf /var/www/smultronfeet.nu
-cp -a build /var/www/smultronfeet.nu
+cp -a build /var/www/smultronfeet.nu --no-preserve=all
 chown -R nginx:nginx /var/www/smultronfeet.nu
